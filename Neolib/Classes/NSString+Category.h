24f3a55b019a0d6a789d654d913d9f4f5ae1af06
Neo <190735345@qq.com>
2016-05-30 09:52:13 +0800

    新增 string category

--- Neolib/Classes/NSString+Category.h
+++ Neolib/Classes/NSString+Category.h
@@ -48,6 +48,52 @@
 
 - (NSDate *)dateFromFormat: (NSString *)formatter ;
 
+#pragma mark - Extension
+/**
+ *  将image进行base64编码
+ */
++ (NSString *)imageBase64:(UIImage *)image;
+
+/**
+ *  返回单行文字大小
+ *
+ *  @param fontSize 字体大小
+ */
+- (CGSize)titleSizeWithfontSize:(CGFloat)fontSize;
+
+/**
+ *  返回文字大小
+ *
+ *  @param fontSize 字体大小
+ */
+- (CGSize)titleSizeWithfontSize:(CGFloat)fontSize maxSize:(CGSize)maxSize;
+
+/**
+ *  根据时间戳字符串返回指定格式的时间字符串
+ *
+ *  @param createTime 时间戳
+ *
+ *  @return 返回时间字符串
+ */
++ (NSString *)dateStringWithCreateTimeFromString:(NSString *)createTime;
+
+/**
+ *  根据时间戳字符串返回指定格式的年月日字符串
+ *
+ *  @param createTime 时间戳
+ *
+ *  @return 返回时间字符串
+ */
++ (NSString *)nyrDateStringWithCreateTimeFromString:(NSString *)createTime;
+
+/**
+ *  根据指定格式的年月日字符串返回时间戳字符串
+ *
+ *  @param nyrDateString 年月日时间字符串
+ *
+ *  @return 返回时间字符串
+ */
++ (NSString *)createDateStringWithNYRDateString:(NSString *)nyrDateString;
 
 @end
 
